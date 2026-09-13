6db2b0badb9a08d720e9589b9b47254f1a3db5d2
AlunoHenryRodrigues <henry.20260114@institutojef.org.br>
2026-09-13 15:33:42 -0300

    feat: capa completa

--- capa.js
+++ capa.js
@@ -4,12 +4,12 @@ const right = document.getElementById("direita");
 
 //processamentos e funções
 direita.addEventListener("click", function(){
-    window.location.href = "slide1.html"
+    window.location.href = "slide1.html" //muda pro arquivo que for o proximo slide
 });
 
 document.addEventListener("keydown", function(event){
     if (event.key === "ArrowRight"){
-        window.location.href = "slide1.html"
+        window.location.href = "slide1.html" //muda pro arquivo que for o proximo slide
     }
 })
 
